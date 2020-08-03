621f0bb490c383bfd8d0432c45e4c5f902fab3bc
SHINMH <sky95012@gmail.com>
2020-08-04 02:30:21 +0900

    Apply gatsby-starter-bee template to my blog
    Apply gatsby-starter-bee template to my blog.

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `SHINMH.dev`,
+  description: `프로그래밍, 알고리즘, 관심있는 분야에 대해 포스팅합니다.`,
+  author: `SHINMH`,
+  introduction: `프로그래밍, 알고리즘, 관심있는 분야에 대해 포스팅합니다.`,
+  siteUrl: `https://shinmh.dev`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `SHINMH`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'SHINMH/git-blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'shinmh',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'UA-173908320-1', // Add your google analytics tranking ID
+}
